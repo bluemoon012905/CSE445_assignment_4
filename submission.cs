5d4fab1186fdd29300ece8f015d5642c1079e2b7
bluemoon012905 <blue.yuda.wang@gmail.com>
2025-10-29 20:29:37 -0700

    fix 1

--- submission.cs
+++ submission.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Xml.Schema;
 using System.Xml;
 using Newtonsoft.Json;
@@ -19,63 +19,71 @@ namespace ConsoleApp1
 
     public class Program
     {
-        public static string xmlURL = "https://raw.githubusercontent.com/reverydev/cse445-assignment4/main/Hotels.xml";
-        public static string xmlErrorURL = "https://raw.githubusercontent.com/reverydev/cse445-assignment4/main/HotelsErrors.xml";
-        public static string xsdURL = "https://raw.githubusercontent.com/reverydev/cse445-assignment4/main/Hotels.xsd";
-
-        public static void Main(string[] args)
-        {
-            string result = Verification(xmlURL, xsdURL);
-            Console.WriteLine(result);
-
-
-            result = Verification(xmlErrorURL, xsdURL);
-            Console.WriteLine(result);
-
-
-            result = Xml2Json(xmlURL);
-            Console.WriteLine(result);
-        }
-
-        // Q2.1
-        public static string Verification(string xmlUrl, string xsdUrl)
-        {
-            try
-            {
-                XmlReaderSettings settings = new XmlReaderSettings();
-                settings.Schemas.Add(null, xsdUrl);
-                settings.ValidationType = ValidationType.Schema;
-                XmlReader reader = XmlReader.Create(xmlUrl, settings);
-                while (reader.Read()) ;
-                return "No Error";
-            }
-            catch (XmlSchemaValidationException ex)
-            {
-                return ex.Message;
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-            }
-        }
-
-        public static string Xml2Json(string xmlUrl)
-        {
-            try
-            {
-                System.Net.WebClient wc = new System.Net.WebClient();
-                string xml = wc.DownloadString(xmlUrl);
-                XmlDocument doc = new XmlDocument();
-                doc.LoadXml(xml);
-                string jsonText = JsonConvert.SerializeXmlNode(doc);
-                return jsonText;
-            }
-            catch (Exception ex)
-            {
-                return ex.Message;
-            }
-
-        }
-    }
+                public static void Main(string[] args)
+                {
+                    string xmlURL = args[0];
+                    string xsdURL = args[1];
+                    string xmlErrorURL = args[2];
+        
+                    string result = Verification(xmlURL, xsdURL);
+                    Console.WriteLine(result);
+        
+        
+                    result = Verification(xmlErrorURL, xsdURL);
+                    Console.WriteLine(result);
+        
+        
+                    result = Xml2Json(xmlURL);
+                    Console.WriteLine(result);
+                }        
+                // Q2.1
+                /// <summary>
+                /// Verifies an XML file against an XSD schema.
+                /// </summary>
+                /// <param name="xmlUrl">The URL or local path of the XML file.</param>
+                /// <param name="xsdUrl">The URL or local path of the XSD schema file.</param>
+                /// <returns>"No Error" if validation is successful, otherwise returns the error message.</returns>
+                public static string Verification(string xmlUrl, string xsdUrl)
+                {
+                    try
+                    {
+                        XmlReaderSettings settings = new XmlReaderSettings();
+                        settings.Schemas.Add(null, xsdUrl);
+                        settings.ValidationType = ValidationType.Schema;
+                        XmlReader reader = XmlReader.Create(xmlUrl, settings);
+                        while (reader.Read()) ;
+                        return "No Error";
+                    }
+                    catch (XmlSchemaValidationException ex)
+                    {
+                        return ex.Message;
+                    }
+                    catch (Exception ex)
+                    {
+                        return ex.Message;
+                    }
+                }
+        
+                /// <summary>
+                /// Converts an XML file to a JSON string.
+                /// </summary>
+                /// <param name="xmlUrl">The URL or local path of the XML file.</param>
+                /// <returns>A JSON string representation of the XML file, or an error message if conversion fails.</returns>
+                public static string Xml2Json(string xmlUrl)
+                {
+                    try
+                    {
+                        string xml = File.ReadAllText(xmlUrl);
+                        XmlDocument doc = new XmlDocument();
+                        doc.LoadXml(xml);
+                        string jsonText = JsonConvert.SerializeXmlNode(doc);
+                        return jsonText;
+                    }
+                    catch (Exception ex)
+                    {
+                        return ex.Message;
+                    }
+        
+                }    }
 
 }
